58335ed8f299c06ade86e4d2891d390cb2fd1f37
Sheridan Kaelee <161584454+lazylianos@users.noreply.github.com>
2024-02-28 12:02:54 +0200

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,25 @@
+int jumpSearch(int arr[], int x, int n) {
+    // Finding block size to be jumped
+    int step = sqrt(n);
+ 
+    // Finding the block where element is
+    // present (if it is present)
+    int prev = 0;
+    while (arr[min(step, n)-1] < x)
+    {
+        prev = step;
+        step += sqrt(n);
+        if (prev >= n)
+            return -1;
+
+#include <iostream>
+using namespace std;
+ 
+int search(int arr[], int N, int x) {
+    int i;
+    for (i = 0; i < N; i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
+}
+ 
